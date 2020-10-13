1129dc0d1fb7016cb91432f28a1170250bac0dc2
makhlouf279 <70649716+makhlouf279@users.noreply.github.com>
2020-10-12 23:38:53 -0400

    cleaning data

--- Data_Wrangling.py
+++ Data_Wrangling.py
@@ -1,7 +1,12 @@
 import pandas as pd
-import matplotlib.pylab as plt
+
+import matplotlib as plt
+from matplotlib import pyplot
 from IPython.display import display
 import numpy as np
+
+
+
 df=pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data")
 print(df.shape)
 print(df.columns)
@@ -116,17 +121,131 @@ df.dropna(subset=["price"], axis=0, inplace=True)
 
 # reset index, because we droped two rows
 df.reset_index(drop=True, inplace=True)    
-
 print(df.head())
 
 #Good! Now, we obtain the dataset with no missing values.
+#The last step in data cleaning is checking and 
+#making sure that all data is in the correct format 
+#(int, float, text or other).
+
+print(df.dtypes)
+
+df[["bore", "stroke"]] = df[["bore", "stroke"]].astype("float")
+df[["normalized-losses"]] = df[["normalized-losses"]].astype("int")
+df[["price"]] = df[["price"]].astype("float")
+df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
+
+print(df.dtypes)
+
+#Data Standardization
+print(df.head())
+
+df['city-L/100km']=235/df["city-mpg"]
+print(df.head())
+
+df['highway-L/100km']=235/df["highway-mpg"]
+# rename column name from "highway-mpg" to "highway-L/100km"
+df.rename(columns={'"highway-mpg"':'highway-L/100km'}, inplace=True)
+print(df.head())
+
+
+#Data Normalization
+
+# replace (original value) by (original value)/(maximum value)
+df['length'] = df['length']/df['length'].max()
+df['width'] = df['width']/df['width'].max()
+
+# Write your code below and press Shift+Enter to execute 
+df['height'] = df['height']/df['height'].max() 
+# show the scaled columns
+print(df[["length","width","height"]].head())
+
+#Binning
+#Convert data to correct format
+df["horsepower"]=df["horsepower"].astype(int, copy=True)
+
+
+#Lets plot the histogram of horspower, to see what the distribution of horsepower looks like.
+plt.pyplot.hist(df["horsepower"])
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
 
 
 
+bins = np.linspace(min(df["horsepower"]), max(df["horsepower"]), 4)
+print(bins)
+
+#We set group names:
+group_names = ['Low', 'Medium', 'High']
+
+#We apply the function "cut" the determine what each value of "df['horsepower']" belongs to.
+df['horsepower-binned'] = pd.cut(df['horsepower'], bins, labels=group_names, include_lowest=True )
+print(df[['horsepower','horsepower-binned']].head(20))
+
+#Lets see the number of vehicles in each bin.
+
+print(df["horsepower-binned"].value_counts())
+
+#Lets plot the distribution of each bin.
+pyplot.bar(group_names, df["horsepower-binned"].value_counts())
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+#Bins visualization
+
+# draw historgram of attribute "horsepower" with bins = 3
+plt.pyplot.hist(df["horsepower"], bins = 3)
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
 
 
+#Indicator variable (or dummy variable)
+print(df.columns)
+
+#get indicator variables and assign it to data frame "dummy_variable_1"
+dummy_variable= pd.get_dummies(df["fuel-type"])
+print(dummy_variable.head())
 
 
+#change column names for clarity
+
+dummy_variable.rename(columns={'gas':'fuel-type-gas', 'diesel':'fuel-type-diesel'}, inplace=True)
+print(dummy_variable.head())
+
+# merge data frame "df" and "dummy_variable" 
+df = pd.concat([df, dummy_variable], axis=1)
+
+# drop original column "fuel-type" from "df"
+df.drop("fuel-type", axis = 1, inplace=True)
+print(df.head())
+
+
+# get indicator variables of aspiration and assign it to data frame "dummy_variable_2"
+dummy_variable2 = pd.get_dummies(df['aspiration'])
+
+# change column names for clarity
+dummy_variable2.rename(columns={'std':'aspiration-std', 'turbo': 'aspiration-turbo'}, inplace=True)
+
+# show first 5 instances of data frame "dummy_variable_1"
+print(dummy_variable2.head())
+
+# merge data frame "df" and "dummy_variable_2" 
+df = pd.concat([df, dummy_variable2], axis=1)
+
+# drop original column "aspiration" from "df"
+df.drop("aspiration", axis = 1, inplace=True)
+print(df.head())
+
+#save the new csv 
+
+df.to_csv('clean_df.csv')
 
 
 
